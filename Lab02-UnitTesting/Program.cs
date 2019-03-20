ff309ff00b48c9d3ad204b891c9129bb635d543a
Chris <chrismorton.cm@gmail.com>
2019-03-20 12:06:31 -0700

    Im moving on to the second test for withdrawal.

--- Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Program.cs
@@ -2,15 +2,28 @@
 
 namespace Lab02_UnitTesting
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            decimal balance = 2400.00M;
-            Console.WriteLine("Welcome to Bank Name.");
-            Console.WriteLine("How can we help you today?");
-            decimal change = Choices(balance);
-            balance += change;
+            try
+            {
+                decimal balance = 2400.00M;
+                Console.WriteLine("Welcome to Bank Name.");
+                Console.WriteLine("How can we help you today?");
+                decimal change = Choices(balance);
+                balance += change;
+                if (change != 0)
+                {
+                    Console.WriteLine($"Your balance is { balance}.");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
 
         }
         static decimal Choices(decimal balance)
@@ -30,7 +43,33 @@ namespace Lab02_UnitTesting
                 }
                 if (chosen == 2)
                 {
-                    Withdraw(balance);
+                    try
+                    {
+                        Console.WriteLine("How much would you like to withdraw?");
+                        string withdrawal = Console.ReadLine();
+                        decimal decimalWithdrawal = Convert.ToDecimal(withdrawal);
+                        Withdraw(balance, decimalWithdrawal);
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("I'm sorry, we could not understand your selection.");
+                        Console.WriteLine("To return to the previous menu, or to exit, please type 'return' or 'exit', respectively.");
+                        Console.WriteLine("How much would you like to withdraw?");
+                        string withdrawal = Console.ReadLine();
+                        if (withdrawal == "return")
+                        {
+                            Choices(balance);
+                        }
+                        if (withdrawal == "exit")
+                        {
+                            return 0;
+                        }
+                        else
+                        {
+                            decimal decimalWithdrawal = Convert.ToDecimal(withdrawal);
+                            Withdraw(balance, decimalWithdrawal);
+                        }
+                    }
                 }
                 if (chosen == 3)
                 {
@@ -41,19 +80,22 @@ namespace Lab02_UnitTesting
                     return 0;
                 }
             }
-            catch (Exception)
+            catch (FormatException  e)
             {
+                Console.WriteLine(e.Message);
                 Console.WriteLine("We could not understand your selection, please try again.");
                 Choices(balance);
             }
+ 
         }
         static decimal Balance(decimal balance)
         {
-            return balance;
+            Console.WriteLine(balance);
+            return 0;
         }
-        static decimal Withdraw(decimal balance)
+        public static decimal Withdraw(decimal balance, decimal withdrawal)
         {
-
+            return balance - withdrawal;
         }
         static decimal Deposit(decimal balance)
         {
--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using static Lab02_UnitTesting.Program;
 
 namespace XUnitTestProject1
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ICanWithdrawFromTheBalance()
         {
+            decimal mockBalance = 2400.00M;
+            decimal mockWithdraw = 60.00M;
 
+            decimal result = Withdraw(mockBalance, mockWithdraw);
+
+            Assert.Equal(2340.00M, result);
         }
     }
 }
